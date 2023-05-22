ccc0f507803cf7cdbff91663898401da3124e052
DylanKimm <yirang1975@gmail.com>
2023-05-22 12:42:01 -0700

    oo

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-class main{
-    print("Welcome to the Valorant Stink Tracker! \n" )
+import requests
+print("Welcome to the Valorant Stink Tracker! \n" )
     
-    name = input("What is your username? Make sure to be case sensitive! \n")
-    tag = input("What is your tagline? Make sure to be case sensitive! \n")
-    
-    
-    
-}
+name = input("What is your username? Make sure to be case sensitive! \n")
+tag = input("What is your tagline? Make sure to be case sensitive!\n ")
+print(" ")
+
+extension = ((name)+"/:"+(tag))
+
+url = "/valorant/v3/matches/:region/:"
+
+url = url+extension 
